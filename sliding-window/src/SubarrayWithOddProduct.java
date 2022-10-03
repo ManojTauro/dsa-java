6f0da1c22974d80950436d6813d8af21c9767c92
manu310397 <manojb912@gmail.com>
2022-10-03 07:02:43 +0530

    Implemented Sub array with odd product

---
+++ sliding-window/src/SubarrayWithOddProduct.java
@@ -0,0 +1,23 @@
+public class SubarrayWithOddProduct {
+    private static final int[] input = {12, 15, 7, 3, 25, 6, 2, 1, 1, 7};
+
+    public static void main(String[] args) {
+        int result = 0;
+        int prod = 1;
+
+        int left=0, right = 0;
+
+        while (right < input.length) {
+            prod *= input[right];
+
+            while (prod % 2 == 0)
+                prod /= input[left++];
+
+            result += right - left + 1;
+
+            right++;
+        }
+
+        System.out.println("Sub Array count = "+result);
+    }
+}
