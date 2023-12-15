5d29b6fdb762c7236093ce98a194f829bc2dd093
manojb912 <manojb912@gmail.com>
2023-12-15 10:19:32 +0000

    Implemented Fib using DP

--- dynamic-programming/src/com/manoj/memoization/fib/Fib.java
+++ dynamic-programming/src/com/manoj/memoization/fib/Fib.java
@@ -4,51 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Fib {
-    private static Map<Long, Long> memo = new HashMap<>();
-
     public static void main(String[] args) {
-        System.out.println(fibRecursive(10));
-//       printFib(5, 1, 1);
+        Map<Integer, Long> memo = new HashMap<>();
+
+        System.out.println(fib(7, memo));
+        System.out.println(fib(8, memo));
+        System.out.println(fib(9, memo));
+        System.out.println(fib(50, memo));
     }
 
-    private static void callDP() {
-        System.out.println(fibDP(6, memo));
-        System.out.println(fibDP(7, memo));
-        System.out.println(fibDP(8, memo));
-        System.out.println(fibDP(50, memo));
-    }
+    private static long fib(int n, Map<Integer, Long> memo) {
+        if (memo.containsKey(n)) return memo.get(n);
+        if (n <= 2) return 1;
 
-    private static long fibRecursive(int n) {
-        if(n<=2) return 1;
-
-        return fibRecursive(n-1) + fibRecursive(n-2);
-    }
-
-    private static long fibDP(long n, Map<Long, Long> memo) {
-        if(memo.containsKey(n)) return memo.get(n);
-
-        if(n<=2) return 1;
-
-        long result = fibDP(n-1, memo) + fibDP(n-1, memo);
-
-        memo.put(n, result);
-
-        return result;
-    }
-
-    //len - max length of the series
-    //curElement - current element in the series
-    //curLength - Current length of the series
-    private static void printFib(int len, int curElement, int curLength) {
-        if(curElement > len) return;
-        if(curElement <=2) {
-            System.out.print(1+" ");
-//            return;
-        }
-
-        System.out.print(curElement+" ");
-
-        printFib(len,  curElement + 1, curLength+1);
-        printFib(len,  curElement + 2, curLength+1);
+        memo.put(n, fib(n - 1, memo) + fib(n - 2, memo));
+        return memo.get(n);
     }
 }
--- dynamic-programming/src/com/manoj/tabulation/subsequences/LongestCommonSubsequence.java
+++ dynamic-programming/src/com/manoj/tabulation/subsequences/LongestCommonSubsequence.java
@@ -2,10 +2,10 @@ package com.manoj.tabulation.subsequences;
 
 //https://www.youtube.com/watch?v=-zI4mrF2Pb4&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY&index=27
 public class LongestCommonSubsequence {
-    private static final String s1 = "abc";
-    private static final String s2 = "abc";
+    private static final String s1 = "ABCBD";
+    private static final String s2 = "BACAD";
     private static int[][] dp = null;
-    
+
     public static void main(String[] args) {
         System.out.println(lcs(s1.toCharArray(), s2.toCharArray()));
 
@@ -15,22 +15,22 @@ public class LongestCommonSubsequence {
     private static int lcs(char[] s1, char[] s2) {
         int m = s1.length;
         int n = s2.length;
-        dp = new int[m+1][n+1];
+        dp = new int[m + 1][n + 1];
 
         //base case
-        for(int i=0; i<=m; i++) dp[i][0] = 0;
-        for(int j=0; j<=n; j++) dp[0][j] = 0;
+        for (int i = 0; i <= m; i++) dp[i][0] = 0;
+        for (int j = 0; j <= n; j++) dp[0][j] = 0;
 
-        for(int i=1; i<=m; i++) {
-            for(int j=1; j<=n; j++) {
-                if(s1[i-1] == s2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
-                else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1[i - 1] == s2[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
 
         return dp[m][n];
     }
-    
+
     private static String getLCS() {
         char[] t1 = s1.toCharArray();
         char[] t2 = s2.toCharArray();
@@ -39,17 +39,16 @@ public class LongestCommonSubsequence {
         StringBuilder builder = new StringBuilder();
         builder.setLength(len);
 
-        int index = len-1;
+        int index = len - 1;
         int i = t1.length;
         int j = t2.length;
 
-        while (i>0 && j>0) {
-            if(t1[i-1] == t2[j-1]) {
-                builder.setCharAt(index--, t1[i-1]);
+        while (i > 0 && j > 0) {
+            if (t1[i - 1] == t2[j - 1]) {
+                builder.setCharAt(index--, t1[i - 1]);
                 i--;
                 j--;
-            }
-            else if(t1[i-1] > t2[j-1]) i--;
+            } else if (t1[i - 1] > t2[j - 1]) i--;
             else j--;
         }
 
